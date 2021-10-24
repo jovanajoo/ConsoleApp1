9b9fa671923485d65cf80751db3a0444783a1bd8
Jovana Bulatovic <bulatovicj07@gmail.com>
2021-10-24 18:12:00 +0200

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int numberOfElement = 1000000;
+
+            Stopwatch linkedTimer = new Stopwatch();
+            int[] arrl = new int[numberOfElement];
+
+           
+
+            measureArrayPerformance(arrl);
+            Console.ReadLine();
+
+        }
+
+        static void measureArrayPerformance(int[] arrl)
+        {
+            var random = new Random();
+            Stopwatch arrayTimer = new Stopwatch();
+            arrayTimer.Start();
+
+            for (int i = 0; i < arrl.Length ; i++)
+            {
+                arrl[i] = random.Next(1000000);
+            }
+            Console.WriteLine("ELement niza: " + arrl[1]);
+            arrayTimer.Stop();
+            Console.WriteLine("Za dodavanje lemenata array niza i trazenje odredjenog elementa potrebno je {0}ms", arrayTimer.ElapsedMilliseconds);
+        }
+    }
+}
