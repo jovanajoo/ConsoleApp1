85de25f5466318b569a991eb8bcde0a58ba54cd7
Jovana Bulatovic <bulatovicj07@gmail.com>
2021-10-26 21:43:24 +0200

    Commit

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -12,19 +12,22 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            const int numberOfElement = 1000000;
-
+            const int numberOfElements = 1000000;
             Stopwatch linkedTimer = new Stopwatch();
-            int[] arrl = new int[numberOfElement];
 
-           
+            int[] arrl = new int[numberOfElements];
 
-            measureArrayPerformance(arrl);
+            LinkedList<int> linkedL = new LinkedList<int>();
+
+            HashSet<int> hashS = new HashSet<int>();
+
+            MeasureArrayPerformance(arrl);
+            MeasureLinkedListPerformance(linkedL);
+            MeasureHashSetPerformance(hashS);
+
             Console.ReadLine();
-
         }
-
-        static void measureArrayPerformance(int[] arrl)
+        static void MeasureArrayPerformance(int[] arrl)
         {
             var random = new Random();
             Stopwatch arrayTimer = new Stopwatch();
@@ -32,11 +35,52 @@ namespace ConsoleApp1
 
             for (int i = 0; i < arrl.Length ; i++)
             {
-                arrl[i] = random.Next(1000000);
+                arrl[i] = random.Next(1234);
             }
-            Console.WriteLine("ELement niza: " + arrl[1]);
+            Console.WriteLine("Element of array: " + arrl[1]);
             arrayTimer.Stop();
-            Console.WriteLine("Za dodavanje lemenata array niza i trazenje odredjenog elementa potrebno je {0}ms", arrayTimer.ElapsedMilliseconds);
+
+            Console.WriteLine("Time for array: {0}ms", arrayTimer.ElapsedMilliseconds);
+        }
+
+        static void MeasureLinkedListPerformance(LinkedList<int> linkedL)
+        {
+            var random = new Random();
+            Stopwatch linkedTimer = new Stopwatch();
+            linkedTimer.Start();
+      
+            for(int i = 0; i < 1000000; i++)
+            {
+                linkedL.AddFirst(random.Next(1234));
+            }
+
+            int temp = linkedL.FirstOrDefault(x => x == 10 ); 
+            Console.WriteLine("Element of LinkedList: " +temp);
+
+            linkedTimer.Stop();
+
+            Console.WriteLine("Time for LinkedList: {0}ms", +linkedTimer.ElapsedMilliseconds);
+        }
+
+        static void MeasureHashSetPerformance(HashSet<int> hashS)
+        {
+            var random = new Random();
+            Stopwatch hashTimer = new Stopwatch();
+            hashTimer.Start();
+
+            for (int i = 0; i < 1000000; i++)
+            {
+                hashS.Add(random.Next(1234));
+            }
+
+            int temp = hashS.FirstOrDefault(x => x == 12);
+            Console.WriteLine("Element of HashSet: " + temp);
+
+            hashTimer.Stop();
+
+            Console.WriteLine("Time for HashSet: {0}ms", +hashTimer.ElapsedMilliseconds);
+
+
         }
     }
 }
